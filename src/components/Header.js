84a05727405c1881fc9fdff1cacb0649c0da2c71
Thosyn Pax <thosynpax@users.noreply.github.com>
2024-09-05 10:16:08 +0100

    removed pax-trail from the menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,6 @@ const Header = () => {
             <li><a href="/projects">Projects</a></li>
             <li><a href="/tech-blog">Tech Blog</a></li>
             <li><a href="https://thosynpax.com/community">Afriheals Careers</a></li>
-            <li><a href="">Pax Trail</a></li>
             <li><a href="/media-kits">Media Kits</a></li>
           </ul>
         </nav>
