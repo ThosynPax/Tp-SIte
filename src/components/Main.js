c4a4d123a6da9c6f52fdacc1aa822addf7884cd8
Thosyn Pax <thosynpax@users.noreply.github.com>
2024-10-17 19:32:48 +0100

    redo main media pages

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,11 +14,8 @@ const Main = () => {
             <header className="intro">
               <h1 className="intro-title">Hey! I'm Thosyn Pax</h1>
               <h4 className="intro-current-job">
-                Transforming Ideas into Reality as a Technical Product Manager,
-                UX Unicorn, & Software Engineer
+                I am a Software Engineer & UI/UX Engineer, aspiring to become a Technical Product Manager
               </h4>
-           
-
             </header>
             <div className="brief-content">
               <p>
