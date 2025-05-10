b769936cbce4681da98f9951adaccb48f1f4e935
Thosyn Pax <thosynpax@users.noreply.github.com>
2025-05-10 19:32:16 +0100

    adjusted footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,12 +26,7 @@ const Footer = () => {
               <BsGithub />
             </a> 
           </div>
-        <p>&copy; {currentYear} Thosyn Pax. All rights reserved. <br />
-        Designed in  
-        <a target="_blank" rel="noopener noreferrer"  href='https://figma.com/' style={{ color: "#fff"}} className='com-3'> Figma</a> and coded in 
-        <a target="_blank" rel="noopener noreferrer" href='https://code.visualstudio.com/' style={{ color: "#fff"}} className='com-3'> Visual Studio Code </a> 
-        by yours truly. Built with 
-        <a target="_blank" rel="noopener noreferrer" href='https://react.dev/' style={{ color: "#fff"}} className='com-3'> React.js</a> and CSS.</p>
+        <p>&copy; {currentYear} Thosyn Pax. All rights reserved. (V6) <br /> </p>
       </div>
     </footer>
   );
